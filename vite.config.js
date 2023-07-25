37b2d02312c29aab21e77c9a18cce322b01aa89e
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2023-07-25 14:11:14 -0600

    Fixed proxy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:40002/",
+        target: "http://localhost:5000/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: true
