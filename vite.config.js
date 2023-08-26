50e5093db7e6a26a2abc258fa36f6426a610074d
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2023-08-25 20:01:43 -0600

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        //target: "https://backend.eglo.pw",
-        target: "http://localhost:5000/",
+        target: "https://backend.eglo.pw",
+        //target: "http://100.69.124.41:5000/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: true,
